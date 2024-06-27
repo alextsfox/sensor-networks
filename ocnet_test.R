647891cd13972b84a07ab4fd105aed7ec4f5f357
Alex Fox <alextsfox@gmail.com>
2024-06-27 15:58:05 -0400

    messing around

--- ocnet_test.R
+++ ocnet_test.R
@@ -23,26 +23,28 @@ data(OCN_400_Allout)
 draw_simple_OCN(OCN_400_Allout)
 
 # messing around
+# messing around
 set.seed(1)
 OCN <- create_OCN(
-    dimX=100, dimY=100,
-    nOutlet=6, 
-    outletSide=c(rep("S", 5), "E"),
-    outletPos=c(as.integer(seq(1, 50, length.out=5)), 25),
-    typeInitialState="I", #T, V, H
-    nIter=40*100*100,
-    showIntermediatePlots=TRUE, nUpdates=20, easyDraw=TRUE,
-    displayUpdates=2,
-    # how long to keep the river network "hot"/plastic for
-    initialNoCoolingPhase=0.5, 
-    # how quickly to cool down/freeze the network once cooling starts.
-    coolingRate=10^5,
+  dimX=30, dimY=120,
+  nOutlet=2, 
+  outletSide=c("E", "W"),
+  outletPos=c(12, 110),
+  typeInitialState="T", #T, V, H
+  showIntermediatePlots=TRUE, nUpdates=20, easyDraw=TRUE,
+  displayUpdates=2,
+  # how long to keep the river network "hot"/plastic for
+  # initialNoCoolingPhase=0, 
+  # how quickly to cool down/freeze the network once cooling starts.
+  # coolingRate=0.3,
 )
 
 draw_simple_OCN(OCN)
 
 river <- landscape_OCN(OCN)
+draw_contour_OCN(river)
 draw_elev2D_OCN(river)
 
 catchments <- aggregate_OCN(river)
-draw_subcatchments_OCN(catchments)
+draw_thematic_OCN(catchments, catchments$AG$A)
+draw_subcatchments_OCN(catchments, colPalette = hcl.colors(5, "Viridis"))
